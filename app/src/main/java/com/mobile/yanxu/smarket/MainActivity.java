6656e186c8c226a23409f4238996f9736bae6b2d
yanxu <yanxu.zheng.au@gmail.com>
2014-09-12 18:09:57 +1000

    clear unused code

--- app/src/main/java/com/mobile/yanxu/smarket/MainActivity.java
+++ app/src/main/java/com/mobile/yanxu/smarket/MainActivity.java
@@ -4,16 +4,12 @@ package com.mobile.yanxu.smarket;
 
 import android.app.Activity;
 import android.app.ActionBar;
-import android.app.Fragment;
 import android.app.FragmentTransaction;
 import android.support.v13.app.FragmentPagerAdapter;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
 
 public class MainActivity extends Activity implements ActionBar.TabListener {
 
